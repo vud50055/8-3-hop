659691a4d6cb72c9107211f3cdb249f6ca79d16d
mylove <duong200401@gmail.com>
2024-03-07 23:22:33 +0700

    first commit

---
+++ config.js
@@ -0,0 +1,9 @@
+const nameGirl = 'Em';
+const giftUrl = 'http://nodemy.vn';
+const eventName = 'Chúc Mừng 8-3';
+const titleCard = 'Tặng em gái ';
+const contentCard = 'chúc em gái 8/3 vui vẻ mạnh khỏe học giỏi thành công trong cuộc sống';
+
+const giftImage = '429970458_1774212326389640_2713795769927246546_n.jpg';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
